611f142366babc2cc3412a75f18580f36d550970
CohleM <manish981194@gmail.com>
2026-03-02 22:07:27 +0545

    update contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
 
                 <h1 className="text-3xl font-bold mb-4 text-slate-700">Have some query or feedback?</h1>
                 <p></p>
-                If you think there's an unsual behavior in our application, please send us an email at<a href="mailto:contact.primeclip@gmail.com" className='text-indigo-600'> contact.primeclip@gmail.com</a>
+                If you think there's an unsual behavior in our application, please send us an email at<a href="mailto:hello@primeclip.pro" className='text-indigo-600'> hello@primeclip.pro</a>
             </div>
         </div>
     )
